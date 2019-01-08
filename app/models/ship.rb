6bde5f8613e8e58323c208841325e90effa209b3
Samuel Cornet <samuel@noukod.com>
2019-01-08 20:28:17 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -19,20 +19,3 @@ class Ship
   end
 end
 
-class Ship
-  attr_accessor :name, :type, :booty
-  SHIP = []
-
-  def initialize ship
-    @name, @type, @booty = ship[:name], ship[:type], ship[:booty]
-    SHIP << self
-  end
-
-  def self.all
-    SHIP
-  end
-
-  def self.clear
-    SHIP.clear
-  end
-end
