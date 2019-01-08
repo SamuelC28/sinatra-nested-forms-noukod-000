3c9ea1c55539c09bd7a364f34ec56089d5eb606a
Samuel Cornet <samuel@noukod.com>
2019-01-08 20:05:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :new
+    end
+
+  	post '/ship' do
+	      @ship = Ship.new(params[:ship])
+
+	      params[:ship][:pirates].each do |pirate|
+	         @pirate = Pirate.new pirate
+	 	       erb :show
+	      end
+    end
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,12 @@
 class Pirate
+  attr_accessor :name, :weigh, :height
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weights]
+    @height = params[:height]
+    @@ll << self
+    end
 end
